3e82bfef5de05c57b4838f400d315e8802a0d9ce
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-27 17:25:37 -0500

    feat: add homepage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Aside from '@/components/ui/aside';
+import Events from '@/components/events/events';
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="p-[10px] flex flex-col lg:flex-row gap-3">
+      <Aside />
+      <Events />
+    </main>
+  );
 }
