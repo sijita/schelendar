e35d01250aa3ab4a7ee3419b88953936ac81458c
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2025-01-28 20:10:31 -0500

    fix: add suspense to some components to prevent errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,32 @@ import Aside from '@/components/ui/aside';
 import Events from '@/components/events/events';
 import { EditEventModal } from '@/components/modals/edit-event-modal';
 import { AddEventModal } from '@/components/modals/add-event-modal';
+import { Suspense } from 'react';
+import { Spinner } from '@heroui/react';
 
 export default function Home() {
   return (
     <main className="p-[10px] flex flex-col lg:flex-row gap-3">
       <AddEventModal />
-      <EditEventModal />
+      <Suspense
+        fallback={
+          <div className="w-full h-full flex items-center justify-center">
+            <Spinner />
+          </div>
+        }
+      >
+        <EditEventModal />
+      </Suspense>
       <Aside />
-      <Events />
+      <Suspense
+        fallback={
+          <div className="w-full h-full flex items-center justify-center">
+            <Spinner />
+          </div>
+        }
+      >
+        <Events />
+      </Suspense>
     </main>
   );
 }
